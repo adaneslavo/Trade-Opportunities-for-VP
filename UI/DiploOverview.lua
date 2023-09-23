8ebfb3c9bb2702f83aac11df7ba39866a782d6bd
adan_eslavo <adan_eslavo@o2.pl>
2023-09-23 20:26:46 +0200

    InfoAddict

--- UI/DiploOverview.lua
+++ UI/DiploOverview.lua
@@ -1,147 +1,185 @@
 print("This is the modded DiploOverview from 'UI - Trade Opportunities'")
 
-include( "IconSupport" );
+include( "IconSupport" )
 -------------------------------------------------
 -- Diplomatic
 -------------------------------------------------
-
-local m_CurrentPanel = Controls.RelationsPanel;
-local m_PopupInfo = nil;
+local m_CurrentPanel = Controls.GraphPanel
+local m_PopupInfo = nil
 
 -------------------------------------------------
 -- On Popup
 -------------------------------------------------
-function OnPopup( popupInfo )
-
-	if( popupInfo.Type == ButtonPopupTypes.BUTTONPOPUP_DIPLOMATIC_OVERVIEW ) then
-		m_PopupInfo = popupInfo;
-		if( m_PopupInfo.Data1 == 1 ) then
-        	if( ContextPtr:IsHidden() == false ) then
-        	    OnClose();
+function OnPopup(popupInfo)
+	if popupInfo.Type == ButtonPopupTypes.BUTTONPOPUP_DIPLOMATIC_OVERVIEW then
+		m_PopupInfo = popupInfo
+		
+		if m_PopupInfo.Data1 == 1 then
+        	if ContextPtr:IsHidden() == false then
+        	    OnClose()
             else
-            	UIManager:QueuePopup( ContextPtr, PopupPriority.InGameUtmost );
+            	UIManager:QueuePopup(ContextPtr, PopupPriority.InGameUtmost)
         	end
     	else
-        	UIManager:QueuePopup( ContextPtr, PopupPriority.DiploOverview );
+        	UIManager:QueuePopup(ContextPtr, PopupPriority.DiploOverview)
     	end
 	end
 end
-Events.SerialEventGameMessagePopup.Add( OnPopup );
+Events.SerialEventGameMessagePopup.Add(OnPopup)
 
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
-function OnDeals()
+function OnGraph()
 	-- Set Tabs
-	Controls.DealsSelectHighlight:SetHide(false);
-	Controls.RelationsSelectHighlight:SetHide(true);
-	Controls.TradesSelectHighlight:SetHide(true);
-	Controls.CityStatesSelectHighlight:SetHide(true);
-	Controls.GlobalSelectHighlight:SetHide(true);
+	Controls.GraphSelectHighlight:SetHide(false)
+	Controls.RelationsSelectHighlight:SetHide(true)
+	Controls.GlobalSelectHighlight:SetHide(true)
+	Controls.TradesSelectHighlight:SetHide(true)
+	Controls.CityStatesSelectHighlight:SetHide(true)
+	Controls.DealsSelectHighlight:SetHide(true)
 	
 	-- Set Panels
-    Controls.RelationsPanel:SetHide( true );
-    Controls.TradesPanel:SetHide( true );
-    Controls.CityStatesPanel:SetHide( true );
-    Controls.DealsPanel:SetHide( false );
-    Controls.GlobalPanel:SetHide( true );
-    m_CurrentPanel = Controls.DealsPanel;
+    Controls.GraphPanel:SetHide(false)
+    Controls.RelationsPanel:SetHide(true)
+    Controls.GlobalPanel:SetHide(true)
+    Controls.TradesPanel:SetHide(true)
+    Controls.CityStatesPanel:SetHide(true)
+    Controls.DealsPanel:SetHide(true)
+    
+	m_CurrentPanel = Controls.GraphPanel
 end
-Controls.DealsButton:RegisterCallback( Mouse.eLClick, OnDeals );
+Controls.GraphButton:RegisterCallback(Mouse.eLClick, OnGraph)
 
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 function OnRelations()
 	-- Set Tabs
-	Controls.DealsSelectHighlight:SetHide(true);
-	Controls.RelationsSelectHighlight:SetHide(false);
-	Controls.TradesSelectHighlight:SetHide(true);
-	Controls.CityStatesSelectHighlight:SetHide(true);
-	Controls.GlobalSelectHighlight:SetHide(true);
+	Controls.GraphSelectHighlight:SetHide(true)
+	Controls.RelationsSelectHighlight:SetHide(false)
+	Controls.GlobalSelectHighlight:SetHide(true)
+	Controls.TradesSelectHighlight:SetHide(true)
+	Controls.CityStatesSelectHighlight:SetHide(true)
+	Controls.DealsSelectHighlight:SetHide(true)
 	
 	-- Set Panels
-    Controls.RelationsPanel:SetHide( false );
-    Controls.TradesPanel:SetHide( true );
-    Controls.CityStatesPanel:SetHide( true );
-    Controls.DealsPanel:SetHide( true );
-    Controls.GlobalPanel:SetHide( true );
-    m_CurrentPanel = Controls.RelationsPanel;
+    Controls.GraphPanel:SetHide(true)
+    Controls.RelationsPanel:SetHide(false)
+    Controls.GlobalPanel:SetHide(true)
+    Controls.TradesPanel:SetHide(true)
+    Controls.CityStatesPanel:SetHide(true)
+    Controls.DealsPanel:SetHide(true)
+    
+	m_CurrentPanel = Controls.RelationsPanel
 end
-Controls.RelationsButton:RegisterCallback( Mouse.eLClick, OnRelations );
-
-
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-function OnTrades()
-	-- Set Tabs
-	Controls.DealsSelectHighlight:SetHide(true);
-	Controls.RelationsSelectHighlight:SetHide(true);
-	Controls.TradesSelectHighlight:SetHide(false);
-	Controls.CityStatesSelectHighlight:SetHide(true);
-	Controls.GlobalSelectHighlight:SetHide(true);
-	
-	-- Set Panels
-    Controls.RelationsPanel:SetHide( true );
-    Controls.TradesPanel:SetHide( false );
-    Controls.CityStatesPanel:SetHide( true );
-    Controls.DealsPanel:SetHide( true );
-    Controls.GlobalPanel:SetHide( true );
-    m_CurrentPanel = Controls.TradesPanel;
-end
-Controls.TradesButton:RegisterCallback( Mouse.eLClick, OnTrades );
-
-
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
-function OnCityStates()
-	-- Set Tabs
-	Controls.DealsSelectHighlight:SetHide(true);
-	Controls.RelationsSelectHighlight:SetHide(true);
-	Controls.TradesSelectHighlight:SetHide(true);
-	Controls.CityStatesSelectHighlight:SetHide(false);
-	Controls.GlobalSelectHighlight:SetHide(true);
-	
-	-- Set Panels
-    Controls.RelationsPanel:SetHide( true );
-    Controls.TradesPanel:SetHide( true );
-    Controls.CityStatesPanel:SetHide( false );
-    Controls.DealsPanel:SetHide( true );
-    Controls.GlobalPanel:SetHide( true );
-    m_CurrentPanel = Controls.CityStatesPanel;
-end
-Controls.CityStatesButton:RegisterCallback( Mouse.eLClick, OnCityStates );
+Controls.RelationsButton:RegisterCallback(Mouse.eLClick, OnRelations)
 
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 function OnGlobal()
 	-- Set Tabs
-	Controls.DealsSelectHighlight:SetHide(true);
-	Controls.RelationsSelectHighlight:SetHide(true);
-	Controls.TradesSelectHighlight:SetHide(true);
-	Controls.CityStatesSelectHighlight:SetHide(true);
-	Controls.GlobalSelectHighlight:SetHide(false);
+	Controls.GraphSelectHighlight:SetHide(true)
+	Controls.RelationsSelectHighlight:SetHide(true)
+	Controls.GlobalSelectHighlight:SetHide(false)
+	Controls.TradesSelectHighlight:SetHide(true)
+	Controls.CityStatesSelectHighlight:SetHide(true)
+	Controls.DealsSelectHighlight:SetHide(true)
 	
 	-- Set Panels
-    Controls.RelationsPanel:SetHide( true );
-    Controls.TradesPanel:SetHide( true );
-    Controls.CityStatesPanel:SetHide( true );
-    Controls.DealsPanel:SetHide( true );
-    Controls.GlobalPanel:SetHide( false );
-    m_CurrentPanel = Controls.GlobalPanel;
+    Controls.GraphPanel:SetHide(true)
+    Controls.RelationsPanel:SetHide(true)
+    Controls.GlobalPanel:SetHide(false)
+    Controls.TradesPanel:SetHide(true)
+    Controls.CityStatesPanel:SetHide(true)
+    Controls.DealsPanel:SetHide(true)
+    
+	m_CurrentPanel = Controls.GlobalPanel
 end
-Controls.GlobalPoliticsButton:RegisterCallback( Mouse.eLClick, OnGlobal );
+Controls.GlobalPoliticsButton:RegisterCallback(Mouse.eLClick, OnGlobal)
 
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
-local m_AllPanels = {OnRelations, OnTrades, OnCityStates, OnDeals, OnGlobal}
+function OnTrades()
+	-- Set Tabs
+	Controls.GraphSelectHighlight:SetHide(true)
+	Controls.RelationsSelectHighlight:SetHide(true)
+	Controls.GlobalSelectHighlight:SetHide(true)
+	Controls.TradesSelectHighlight:SetHide(false)
+	Controls.CityStatesSelectHighlight:SetHide(true)
+	Controls.DealsSelectHighlight:SetHide(true)
+	
+	-- Set Panels
+    Controls.GraphPanel:SetHide(true)
+    Controls.RelationsPanel:SetHide(true)
+    Controls.GlobalPanel:SetHide(true)
+    Controls.TradesPanel:SetHide(false)
+    Controls.CityStatesPanel:SetHide(true)
+    Controls.DealsPanel:SetHide(true)
+    
+	m_CurrentPanel = Controls.TradesPanel
+end
+Controls.TradesButton:RegisterCallback(Mouse.eLClick, OnTrades)
+
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+function OnCityStates()
+	-- Set Tabs
+	Controls.GraphSelectHighlight:SetHide(true)
+	Controls.RelationsSelectHighlight:SetHide(true)
+	Controls.GlobalSelectHighlight:SetHide(true)
+	Controls.TradesSelectHighlight:SetHide(true)
+	Controls.CityStatesSelectHighlight:SetHide(false)
+	Controls.DealsSelectHighlight:SetHide(true)
+	
+	-- Set Panels
+    Controls.GraphPanel:SetHide(true)
+    Controls.RelationsPanel:SetHide(true)
+    Controls.GlobalPanel:SetHide(true)
+    Controls.TradesPanel:SetHide(true)
+    Controls.CityStatesPanel:SetHide(false)
+    Controls.DealsPanel:SetHide(true)
+    
+	m_CurrentPanel = Controls.CityStatesPanel
+end
+Controls.CityStatesButton:RegisterCallback(Mouse.eLClick, OnCityStates)
+
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+function OnDeals()
+	-- Set Tabs
+	Controls.GraphSelectHighlight:SetHide(true)
+	Controls.RelationsSelectHighlight:SetHide(true)
+	Controls.GlobalSelectHighlight:SetHide(true)
+	Controls.TradesSelectHighlight:SetHide(true)
+	Controls.CityStatesSelectHighlight:SetHide(true)
+	Controls.DealsSelectHighlight:SetHide(false)
+	
+	-- Set Panels
+	Controls.GraphPanel:SetHide(true)
+    Controls.RelationsPanel:SetHide(true)
+    Controls.GlobalPanel:SetHide(true)
+    Controls.TradesPanel:SetHide(true)
+    Controls.CityStatesPanel:SetHide(true)
+    Controls.DealsPanel:SetHide(false)
+    
+	m_CurrentPanel = Controls.DealsPanel
+end
+Controls.DealsButton:RegisterCallback(Mouse.eLClick, OnDeals)
+
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+local m_AllPanels = {OnGraph, OnRelations, OnGlobal, OnTrades, OnCityStates, OnDeals}
 function OnDiploCornerOpenPanel(iIndex)
-  if (ContextPtr:IsHidden()) then
-    m_AllPanels[iIndex]()
-    Events.SerialEventGameMessagePopup({Type=ButtonPopupTypes.BUTTONPOPUP_DIPLOMATIC_OVERVIEW})
-  end
+	if ContextPtr:IsHidden() then
+		m_AllPanels[iIndex]()
+		Events.SerialEventGameMessagePopup({Type=ButtonPopupTypes.BUTTONPOPUP_DIPLOMATIC_OVERVIEW})
+	end
 end
 LuaEvents.DiploCornerOpenPanel.Add(OnDiploCornerOpenPanel)
 
@@ -149,46 +187,46 @@ LuaEvents.DiploCornerOpenPanel.Add(OnDiploCornerOpenPanel)
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 function OnClose()
-    UIManager:DequeuePopup( ContextPtr );
+    UIManager:DequeuePopup(ContextPtr)
 end
-Controls.CloseButton:RegisterCallback( Mouse.eLClick, OnClose);
+Controls.CloseButton:RegisterCallback(Mouse.eLClick, OnClose)
 
 ----------------------------------------------------------------
 -- 'Active' (local human) player has changed
 ----------------------------------------------------------------
-Events.GameplaySetActivePlayer.Add(OnClose);
+Events.GameplaySetActivePlayer.Add(OnClose)
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
-function InputHandler( uiMsg, wParam, lParam )
+function InputHandler(uiMsg, wParam, lParam)
     if uiMsg == KeyEvents.KeyDown then
         if wParam == Keys.VK_ESCAPE or wParam == Keys.VK_RETURN then
-            OnClose();
-            return true;
+            OnClose()
+            return true
         end
     end
 end
-ContextPtr:SetInputHandler( InputHandler );
+ContextPtr:SetInputHandler(InputHandler)
 
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
-function ShowHideHandler( bIsHide, bInitState )
+function ShowHideHandler(bIsHide, bInitState)
 	-- Set player icon at top of screen
-	CivIconHookup( Game.GetActivePlayer(), 64, Controls.Icon, Controls.CivIconBG, Controls.CivIconShadow, false, true );
+	CivIconHookup(Game.GetActivePlayer(), 64, Controls.Icon, Controls.CivIconBG, Controls.CivIconShadow, false, true)
 
-    if( not bInitState ) then
-        if( not bIsHide ) then
-        	UI.incTurnTimerSemaphore();
+    if not bInitState then
+        if not bIsHide then
+        	UI.incTurnTimerSemaphore()
         	-- trigger the show/hide handler to update state
-        	m_CurrentPanel:SetHide( false );
-        	Events.SerialEventGameMessagePopupShown(m_PopupInfo);
+        	m_CurrentPanel:SetHide(false)
+        	Events.SerialEventGameMessagePopupShown(m_PopupInfo)
         else
-        	UI.decTurnTimerSemaphore();
-        	Events.SerialEventGameMessagePopupProcessed.CallImmediate(ButtonPopupTypes.BUTTONPOPUP_DIPLOMATIC_OVERVIEW, 0);
+        	UI.decTurnTimerSemaphore()
+        	Events.SerialEventGameMessagePopupProcessed.CallImmediate(ButtonPopupTypes.BUTTONPOPUP_DIPLOMATIC_OVERVIEW, 0)
         end
     end
 end
-ContextPtr:SetShowHideHandler( ShowHideHandler );
+ContextPtr:SetShowHideHandler(ShowHideHandler)
 
-OnRelations();
+OnRelations()
